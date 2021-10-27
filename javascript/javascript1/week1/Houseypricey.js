1cee7ad84b73e4a209ad2e47db7c4efe024585ca
Mamidi001 <swethahimabindu@gmail.com>
2021-10-27 19:45:54 +0200

    finishedhousepricejs

---
+++ javascript/javascript1/week1/Houseypricey.js
@@ -0,0 +1,23 @@
+//peter house
+let wide = 8;
+let deep = 10;
+let hight = 10;
+let volumeOfHouse= wide*deep*hight;
+let gardenSize = 100;
+let houseCost = 25000000;
+let housePrice = volumeOfHouse * 2.5 * 1000 + gardenSize * 300;
+
+if(housePrice>houseCost){console.log('then he is paying more')}
+else {console.log('he is paying less')};
+
+// about julia's house
+
+let wide =5;
+let deep=11;
+let high = 8;
+let houseVolume = wide*deep*high;
+let gardenSize=70;
+let houseCost=1000000;
+let housePrice = houseVolume * 2.5 * 1000 + gardenSize * 300;
+if(housePrice>houseCost){ console.log ('julia is paying high')}
+else{console.log ('julia is paying less')};
