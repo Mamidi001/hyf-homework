5f1d66a5ec85ef88adaa4598f54d00176940df43
Mamidi001 <swethahimabindu@gmail.com>
2021-10-28 15:01:24 +0200

    modified file of eznamey

--- javascript/javascript1/week1/EzNamey.js
+++ javascript/javascript1/week1/EzNamey.js
@@ -2,5 +2,5 @@ const firstWords = ['happy','smile','fun','party','friends','celebration','easy'
 const secondWords =['google','facebook','twitter','insta','linkedin','watsapp','telegram','snapchat','skype','teams'];
 const randomNumber = Math.floor(Math.random() * 10);
 var startupName = firstWords[randomNumber]+" " + secondWords[randomNumber];
-console.log(`The startup name is ${startupName} contains ${startupName.lenght} characters`);
+console.log(`The startup name is ${'startupName'} contains ${startupName.lenght} characters`);
 
