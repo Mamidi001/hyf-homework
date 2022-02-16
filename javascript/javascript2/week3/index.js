cd6af9e17e612c5afbfb7e59c2cb392bfeae18f5
Mamidi001 <swethahimabindu@gmail.com>
2022-02-16 15:41:45 +0100

    changes modified

--- javascript/javascript2/week3/index.js
+++ javascript/javascript2/week3/index.js
@@ -1,46 +1,25 @@
-/*const numbers =[1,2,3,4,5];
- numbers.push(6);
- numbers.unshift(0);
-console.log(numbers);
-console.log(numbers.length);
-numbers.shift();
-numbers.pop();
-console.log(numbers.length);
-for(let i=0; i<numbers.length;i++){
-    console.log(numbers[i]);
-}
-for (const number of numbers){
-    console.log(number);
-}
-for(const number of numbers){
-console.log(number.toString());
-}
-console.log(numbers.join(';'));
-console.log(numbers.includes());
-
-const myClassmates=[{
-    name:'swetha',
-    age:34,
-}]*/
-const classmates=[{
-    name : 'swetha',
-    age:34,
-}, {
-    name:'shravya',
-    age:26,
-    color: 'red'
-},
-{
-    name:'chunmei',
-    age :33===33,
-} , {}]
-classmates.push('vahab');
+const classmates = [
+  {
+    name: "swetha",
+    age: 34,
+  },
+  {
+    name: "shravya",
+    age: 26,
+    color: "red",
+  },
+  {
+    name: "chunmei",
+    age: 33 === 33,
+  },
+  {},
+];
+classmates.push("vahab");
 console.log(classmates);
-for(const newFriend of classmates){
-    if (newFriend.name==='ali'){
-        console.log('ali is already in the group')
-    }
+for (const newFriend of classmates) {
+  if (newFriend.name === "ali") {
+    console.log("ali is already in the group");
+  }
 }
-classmates[1].age=20;
-console.log(Object.keys(classmates[1]).length ===0);
-
+classmates[1].age = 20;
+console.log(Object.keys(classmates[1]).length === 0);
--- javascript/javascript2/week3/timer.js
+++ javascript/javascript2/week3/timer.js
@@ -1,152 +1,134 @@
- //Log out the text Called after 2.5 seconds 2.5 seconds after the script is loaded.
+//Log out the text Called after 2.5 seconds 2.5 seconds after the script is loaded.
 
- let timeout = setTimeout(()=>{
-    console.log('Called after 2.5 seconds')},2500);
+let timeout = setTimeout(() => {
+  console.log("Called after 2.5 seconds");
+}, 2500);
 
- //Create a function that takes 2 parameters: delay and stringToLog
-        
- function delayFunction(delay, stringToLog){
-          setTimeout(() => {
-            console.log(stringToLog)
-          }, delay * 1000);
-      }
-      delayFunction(5,'appear after 5 sec');
-      
- //Create a button in html. button should work after 5 sec
+//Create a function that takes 2 parameters: delay and stringToLog
 
-      const button =document.getElementById('myBtn');
-      button.addEventListener('click',()=>{
-          delayFunction(5,'appear after 5sec clicking the button');
-          });
+function delayFunction(delay, stringToLog) {
+  setTimeout(() => {
+    console.log(stringToLog);
+  }, delay * 1000);
+}
+delayFunction(5, "appear after 5 sec");
 
- // Create two functions and assign them to two different variables.
-   function earth(){
-       const planet1 = " ";
-        console.log('earth');
-   }
-   function saturn(){
-       const palnet2 = " ";
-      console.log('saturn');
-   }
-     const planetLogFunction =(planetLogFunction)=>{
-        planetLogFunction();
-    }
+//Create a button in html. button should work after 5 sec
 
-   planetLogFunction(earth);
-   planetLogFunction(saturn);
+const button = document.getElementById("myBtn");
+button.addEventListener("click", () => {
+  delayFunction(5, "appear after 5sec clicking the button");
+});
 
-   //Create a function called runAfterDelay. It has two parameters: delay and callback. 
-   //When called the function should wait delay seconds and then call the provided callback function.
+// Create two functions and assign(call) them to two different variables.
+function earth() {
+  planet1 = " ";
+  console.log("earth");
+}
+function saturn() {
+  palnet2 = " ";
+  console.log("saturn");
+}
+const planetLogFunction = (planetLogFunction) => {
+  planetLogFunction();
+};
 
-  function runAfterDelay(delay, callback){
-       setTimeout(callback,delay*1000);
-   }
-   runAfterDelay(4, function(){
-       console.log('should be logged after 4 seconds');
-   });
-   runAfterDelay(1, function(){
-    console.log('😀');
+planetLogFunction(earth);
+planetLogFunction(saturn);
+
+//5 location
+let myBtn2 = document.getElementById("myBtn2");
+let latitudeText = document.getElementById("latitude");
+let longitudeText = document.getElementById("longitude");
+button.addEventListener("click", function (e) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    latitudeText.innerText = latitude.toFixed(2);
+    longitudeText.innerText = longitude.toFixed(2);
+  });
+});
+
+//6 location in map
+let button2 = document.getElementById("myBtn2");
+let locationLink = document.querySelector("#locationlink");
+button2.addEventListener("click", function () {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    locationLink.href = `https://maps.google.com/maps?q=${latitude},${longitude}&hl=es;z=14&amp;output=embed`;
+    locationLink.innerText = `click here to see where are you in the map`;
+  });
+});
+
+//Create a function called runAfterDelay. It has two parameters: delay and callback.
+//When called the function should wait delay seconds and then call the provided callback function.
+
+function runAfterDelay(delay, callback) {
+  setTimeout(callback, delay * 1000);
+}
+runAfterDelay(4, function () {
+  console.log("should be logged after 4 seconds");
+});
+runAfterDelay(1, function () {
+  console.log("😀");
 });
 // double click (somehow it is not working)
- 
- 
- let doubleClick = document.getElementById('body');
- doubleClick.addEventListener('dblclick',function(){
-     console.log('double click');
- });
- 
- 
- function jokeCreator(shouldTellFunnyJoke,logFunnyJoke,logBadJoke){
-     
-     if(shouldTellFunnyJoke ===true){
-        logFunnyJoke();
-     }
-     else{logBadJoke ();
-    }
-    }
-    const logFunnyJoke = () =>{
-        console.log('this is a funny joke');
-    }
-    const logBadJoke =() =>{
-        console.log('this is a bad joke');
-    }
-  jokeCreator(true,logFunnyJoke, logBadJoke);
-  jokeCreator(false,logFunnyJoke, logBadJoke);
 
+const body = document.querySelector("#body");
+document.addEventListener("dblclick", function (e) {
+  console.log("double click");
+});
 
+// funny joke or bad joke
 
-  //Create an array with 3 items. All items should be functions. Iterate through the array and call all the functions.
-  
-  const array = [green(),yellow(),blue()];
-  array.forEach((item)=> (item));
+function jokeCreator(shouldTellFunnyJoke, logFunnyJoke, logBadJoke) {
+  if (shouldTellFunnyJoke === true) {
+    logFunnyJoke();
+  } else {
+    logBadJoke();
+  }
+}
+const logFunnyJoke = () => {
+  console.log("this is a funny joke");
+};
+const logBadJoke = () => {
+  console.log("this is a bad joke");
+};
+jokeCreator(true, logFunnyJoke, logBadJoke);
+jokeCreator(false, logFunnyJoke, logBadJoke);
 
-   function green(){
-       console.log('leaves are green');
-   }
-   function yellow(){
-    console.log('mango is yellow');
- }
- function blue(){
-    console.log('sky is blue');
- }
- //Create a function as a const and try creating a function normally. Call both functions. Read up on this if you are interested:
-     console.log(sum(5,3));
-   
-     const addition1 = function(num1,num2){
-      return num1+ num2;
-   }
-     console.log(addition1(2,3)); // we can't call before appllying conditions
-    
-   
-     function sum(num1,num2) {
-        return num1-num2
-        
-    }
+//Create an array with 3 items. All items should be functions. Iterate through the array and call all the functions.
 
-    const obj = {
-    firstName: 'swetha',
-    lastName : 'kancharla',
-    fullName: function name() {
-        return this.firstName+this.lastName
-    }
+const array = [green(), yellow(), blue()];
+array.forEach((item) => item);
 
-    }
-    console.log(obj.fullName());
-    
- 
+function green() {
+  console.log("leaves are green");
+}
+function yellow() {
+  console.log("mango is yellow");
+}
+function blue() {
+  console.log("sky is blue");
+}
+//Create a function as a const and try creating a function normally. Call both functions. Read up on this if you are interested:
+console.log(sum(5, 3));
 
+const addition1 = function (num1, num2) {
+  return num1 + num2;
+};
+console.log(addition1(2, 3)); // we can't call before appllying conditions
 
+function sum(num1, num2) {
+  return num1 - num2;
+}
 
-
-
-   
-  
-  
- 
-  
-
-
-
-
-
-
-// When this button is clicked the location (latitude, longitude)
-  /*const button2 = document.getElementById('myBtn2');
-  let latText = 
-   button2.addEventListener('click',function(){
-       navigator.geolocation.getCurrentPosition(function(position){
-       let latitude = position.coords.latitude;
-       let longitude= position.coords.longitude;
-       
-       
-       });
-       
-   });
-   
-   let map;
-   function initMap(){
-       map = new google.maps.Map(document.getElementById('map'),{
-           center : {lat : -34.397, ing : 150.644},
-           zoom: 8,
-       });
-   }*/
+const obj = {
+  firstName: "swetha",
+  lastName: "kancharla",
+  fullName: function name() {
+    return this.firstName + this.lastName;
+  },
+};
+console.log(obj.fullName());
