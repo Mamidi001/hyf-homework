d96d49e06df8a0feb4b27f35e69de628fa6f2b90
Mamidi001 <swethahimabindu@gmail.com>
2022-03-14 14:19:37 +0100

    warmup exercise

--- nodejs/week2/homework/warmup/index.js
+++ nodejs/week2/homework/warmup/index.js
@@ -3,4 +3,22 @@ const app = express();
 
 app.get("/", (req, res) => res.send("nodejs week2 homework"));
 
+app.get("/numbers/add", function (req, res) {
+  const first = parseInt(req.query.first);
+  const second = parseInt(req.query.second);
+  const sum = first + second;
+  if (first && second) {
+    res.send(`The sum is: ${sum}`);
+  } else if (first) {
+    res.send(`you entered only first value ${first}`);
+  } else if (second) {
+    res.send(`you entered only first ${second}`);
+  }
+});
+app.get("/numbers/multiply/:first/:second", (req, res) => {
+  const firstValue = parseInt(req.params.first);
+  const secValue = parseInt(req.params.second);
+  res.send(`${firstValue * secValue}`);
+});
+
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
