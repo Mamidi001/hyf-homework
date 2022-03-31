5b983e3ba33f2a7b8da568c73593d5481c7f04e1
Swetha <91112898+Mamidi001@users.noreply.github.com>
2022-03-31 11:53:07 +0200

    Add files via upload

---
+++ react/react1/week2/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TodoList from './TodoList';
+import Timer from './Timer';
+
+function App () {
+  return(
+    <div className='App'>
+    <Timer/>
+      <TodoList/>
+       </div>
+       )
+      }
+export default App;
